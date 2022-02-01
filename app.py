82f56dc14997eb755c4b5a94e6e012dd72ffe674
ttaylordev <ttaylordev@gmail.com>
2022-02-01 01:44:57 -0700

    add opening and runing executables functionality to buttons

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, Text
-from tkinter import *
+# from tkinter import *
 import os
 
 # assign tk as the root of the project
@@ -9,11 +9,20 @@ apps = []
 
 # define functions for buttons
 def addApp():
-    filename= filedialog.askopenfile(initialdir='/', title="Select File", filetypes=(('executables', '*.exe'), ('all files', '*.*')))
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    filename= filedialog.askopenfilename(initialdir='/', title="Select File", filetypes=(('executables', '*.exe'), ('all files', '*.*')))
     apps.append(filename)
     print(filename)
 
-# def runApp():
+    for app in apps:
+        label = tk.Label(frame, text=app, bg='grey')
+        label.pack()
+
+def runApp():
+    for app in apps:
+        os.startfile(app)
 
 # create a canvas
 canvas = tk.Canvas(root, height=720, width=720, bg="#424242")
@@ -29,8 +38,12 @@ frame.place(relwidth=0.975, relheight=0.975, relx=0.0125, rely=0.0125)
 openFile = tk.Button(root, text='Open File', padx=10, pady=5, fg='white', bg='#424242', command=addApp)
 openFile.pack()
 
-runApps = tk.Button(root, text='Run Aapps', padx=10, pady=5, fg='white', bg='#424242')
+runApps = tk.Button(root, text='Run Aapps', padx=10, pady=5, fg='white', bg='#424242', command=runApp)
 runApps.pack()
+
+root.mainloop()
+
+
 
 # bottomframe = Frame(root)
 # bottomframe.pack( side = BOTTOM )
@@ -46,6 +59,3 @@ runApps.pack()
 
 # blackbutton = Button(bottomframe, text="Black", fg="black")
 # blackbutton.pack( side = BOTTOM)
-
-
-root.mainloop()
